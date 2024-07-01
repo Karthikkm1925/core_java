b72dc60de3c6331f7fd0ac1f2fc3118baafb133a
karth <karth@laptop200X>
2024-07-01 16:01:47 +0530

    assignment1

---
+++ core_java/src/com/karthik/assignment1/Developer.java
@@ -0,0 +1,22 @@
+package com.karthik.assignment1;
+
+public class Developer extends Employee{
+	private String proglang;
+	
+	// Constructor
+	public Developer(String name, int empid, int salary, String proglang) {
+		super(name,empid,salary);
+		this.proglang = proglang;
+	}
+
+	public String getProglang() {
+		return proglang;
+	}
+
+	public void setProglang(String proglang) {
+		this.proglang = proglang;
+	}
+	
+	 
+
+}
